8bcabe04022d3cf0f0aea0744a752e548aa06ebd
bassem <basem12683@gmail.com>
2024-01-07 13:32:07 +0300

    ract router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Products,
+  SingleProduct,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      { path: "about", element: <About /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+]);
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <Fragment>
+      <nav>
+        <span className="text-4xl text-primary">MEDERMA</span>
+      </nav>
+      <Outlet/>
+    </Fragment>
+  );
+};
+
+export default HomeLayout;
