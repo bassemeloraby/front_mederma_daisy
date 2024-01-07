1b9daa13171f38ea74339e31dfc4d01c8f6d578f
bassem <basem12683@gmail.com>
2024-01-07 15:17:14 +0300

    links

---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { id: 1, url: "/", text: "home" },
+  { id: 2, url: "about", text: "about" },
+  { id: 3, url: "products", text: "products" },
+ 
+];
+
+const NavLinks = () => {
+  return (
+    <Fragment>
+      {links.map((link) => {
+        const { id, url, text } = link;
+
+        return (
+          <li key={id}>
+            <NavLink className="capitalize" to={url}>
+              {text}
+            </NavLink>
+          </li>
+        );
+      })}
+    </Fragment>
+  )
+}
+
+export default NavLinks
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <Fragment>
     <Header />
-     
+    <Navbar />
+
       <section className="align-element py-20">
           <Outlet />
         </section>
