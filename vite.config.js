fbba1781ce4cc70dd3ddadb3753d36f499d847a8
bassem <basem12683@gmail.com>
2024-01-11 20:47:56 +0300

    proxy:

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  proxy: {
+    "/api": {
+      ws: true,
+      changeOrigin: true,
+      target: "https://rich-blue-llama-vest.cyclic.cloud"
+    }
+  }
 })
